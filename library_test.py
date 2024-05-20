ce7eebeed21429ae70bb64f4c4456d6fbb005ffa
Mokhtar Salem <70936834+MokhtarBaWahal@users.noreply.github.com>
2024-05-20 19:10:10 +0300

    Rename library_test.py to setup.py

--- library_test.py
+++
@@ -1,5 +0,0 @@
-import ros_level as rl
-import utilities
-import middleware 
-
-rl.RosNodeGen("/home/g9/DockerizedPrivateCloud_SDK/ros_level/node_config.yml")
---
+++ setup.py
@@ -0,0 +1,5 @@
+import ros_level as rl
+import utilities
+import middleware 
+
+rl.RosNodeGen("/home/g9/DockerizedPrivateCloud_SDK/ros_level/node_config.yml")
